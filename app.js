cc0accf19dd10c4b281c300d73fbb01198ecc22d
picky123 <38593029+picky123@users.noreply.github.com>
2018-05-02 16:25:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -96,3 +96,45 @@ client.on("guildMemberAdd", function(member) {
     member.addRole(role).catch(console.error);
 });
 
+client.on('message', message => { 
+
+    if (message.content.startsWith(PREFIX + 'Userinfo')) {
+
+        const UserInfo = new Discord.MessageEmbed()
+
+
+            .setAuthor(message.author.username, message.author.avatarURL()) 
+            .setTitle('UserInfo')
+            .setURL('www.google.com') 
+            .setColor('#ff0000M') 
+            .setImage(message.author.avatarURL()) 
+            .setThumbnail(message.author.avatarURL()) 
+
+
+            .addField('Avatar', message.author.avatar, true) 
+            .addField('AvatarURL', message.author.avatarURL({
+                format: 'png'
+            }), true)
+            .addField('AvatarURL', message.author.avatarURL({
+                size: '2048'
+            }), true)
+            .addField('Bot', message.author.bot, true) 
+            .addField('Created At', message.author.createdAt, false)
+            .addField('Discrim', message.author.discriminator, true) 
+            .addField('DMChannel', message.author.dmChannel)
+            .addField('ID', message.author.id) 
+            .addField('Last Message', message.author.lastMessage)
+            .addField('Last Message ID', message.author.lastMessageID) 
+            .addField('Presence', message.author.presence) 
+            .addField('Presence Status', message.author.presence.status) 
+            .addField('Presence Game', message.author.presence.activity.name) 
+            .addField('Tag', message.author.tag) 
+            .addField('Username', message.author.username)
+            .addField('Nick Name', message.guild.member(target).displayName) 
+
+            .setFooter('Requested By', message.author.tag) 
+            .setTimestamp() 
+
+        message.channel.send(UserInfo);
+    }
+});
